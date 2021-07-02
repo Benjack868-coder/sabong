937c65be26ea3fbdd7305abc98b02a70c738643e
Benjamin Sumilhig <benjamin.sumilhig@gmail.com>
2021-07-03 02:43:16 +0800

    murag lami na ikatog

--- sabong/settings.py
+++ sabong/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'channels',
     'system.apps.SystemConfig',
     'derby.apps.DerbyConfig',
     'member.apps.MemberConfig',
